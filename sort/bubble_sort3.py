43ce9d2bb965f6686566b19794c790a144bb80fb
doyeon <doyeon311@gmail.com>
2020-12-06 21:25:03 +0900

    버블 정렬 실습

---
+++ sort/bubble_sort3.py
@@ -0,0 +1,31 @@
+# 버블 정렬 알고리즘 구현하기 (알고리즘 개선 2)
+
+from typing import MutableSequence
+
+def bubble_sort(a: MutableSequence) -> None:
+    """버블정렬(스캔 범위를 제한)"""
+    
+
+    k = 0
+    n = len(a)
+    while k < n-1:
+        last = n-1
+        for j in range(n-1,k,-1):
+            if a[j-1] > a[j]:
+                a[j-1],a[j] = a[j],a[j-1]
+                last = j
+        k = last    
+
+if __name__=="__main__":
+    print('버블 정렬을 수행합니다.')
+    num = int(input('원소 수를 입력하세요: '))
+    x = [None] * num # 원소 수가 num인 배열을 생성
+
+    for i in range(num):
+        x[i] = int(input(f'x[{i}]: '))
+    
+    bubble_sort(x)
+
+    print('오름차순으로 정렬했습니다.')
+    for i in range(num):
+        print(f'x[{i}] = {x[i]}')
