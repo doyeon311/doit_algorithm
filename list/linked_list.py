6c146f0b207ae7b48a965bba4c51bacac7530fd5
doyeon <doyeon311@gmail.com>
2020-12-19 18:31:12 +0900

    실습: doit algorithm 연결리스트, 삭제, 출력, 이터레이터

--- list/linked_list.py
+++ list/linked_list.py
@@ -1,6 +1,6 @@
 # 포인터로 연결 리스트 구현하기
 
-# from __future__ import annotations
+from __future__ import annotations
 from typing import Any, Type
 
 
@@ -61,3 +61,98 @@ class LinkedList:
                 ptr = ptr.next
             ptr.next = self.current = Node(data, None)
             self.no += 1
+
+    def remove_first(self) -> None:
+        """머리 노드를 삭제"""
+        if self.head is not None:       # 리스트가 비어 있지 않으면
+            self.head = self.current = self.head.next
+        self.no -= 1
+
+    def remove_last(self):
+        """꼬리 노드를 삭제"""
+        if self.head is not None:
+            if self.head.next is None:      # 노드가 1개 뿐이라면
+                self.remove_first()         # 머리 노드를 삭제
+            else:
+                ptr = self.head             # 스캔 중인 노드
+                pre = self.head             # 스캔 중인 노드의 앞쪽 노드
+
+                while ptr.next is not None:
+                    pre = ptr
+                    ptr = ptr.next
+
+                pre.next = None             # pre는 삭제 뒤 꼬리 노드
+                self.current = pre
+                self.no -= 1
+
+    def remove(self, p: Node) -> None:
+        """노드 p를 삭제"""
+        if self.head is not None:
+            if p is self.head:      # p가 머리 노드이면
+                self.remove_first()  # 머리 노드를 삭제
+            else:
+                ptr = self.head
+
+                while ptr.next is not p:
+                    ptr = ptr.next
+                    if ptr is None:
+                        return      # p는 리스트에 존재하지 않음
+
+                ptr.next = p.next
+                self.current = ptr
+                self.no -= 1
+
+    def remove_current_node(self) -> None:
+        """주목 노드를 삭제"""
+        self.remove(self.current)
+
+    def clear(self) -> None:
+        """전체 노드를 삭제"""
+        while self.head is not None:    # 전체가 비어 있을 때까지
+            self.remove_first()         # 머리 노드를 삭제
+        self.current = None
+        self.no = 0
+
+    def next(self) -> bool:
+        """주목 노드를 한 칸 뒤로 이동"""
+        if self.current is None or self.current.next is None:
+            return False        # 이동할 수 없음
+        self.current = self.current.next
+        return True
+
+    def print_current_node(self) -> None:
+        """주목 노드를 출력"""
+        if self.current is None:
+            print('주목 노드가 존재하지 않습니다.')
+        else:
+            print(self.current.data)
+
+    def print(self) -> None:
+        """모든 노드를 출력"""
+        ptr = self.head
+
+        while ptr is not None:
+            print(ptr.data)
+            ptr = ptr.next
+
+    def __iter__(self) -> LinkedListIterator:
+        """이터레이터를 반환"""
+        return LinkedListIterator(self.head)
+
+
+class LinkedListIterator:
+    """클래스 LinkedList의 이터레이터용 클래스"""
+
+    def __init__(self, head: Node):
+        self.current = head
+
+    def __iter__(self) -> LinkedListIterator:
+        return self
+
+    def __next__(self) -> Any:
+        if self.current is None:
+            raise StopIteration
+        else:
+            data = self.current.data
+            self.current = self.current.next
+            return data
